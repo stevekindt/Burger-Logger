c7898186fb30951d6ad6257d59113567d6a529b8
Steve Kindt <stevemkindt@gmail.com>
2020-03-21 17:32:45 -0500

    orm functions

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,15 @@
+// Set up MySQL connection.
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-  port: 3306,
   host: "localhost",
+  port: 3306,
   user: "root",
   password: "",
   database: "burgers_db"
 });
 
+// Make connection.
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
@@ -16,4 +18,5 @@ connection.connect(function(err) {
   console.log("connected as id " + connection.threadId);
 });
 
+// Export connection for our ORM to use.
 module.exports = connection;

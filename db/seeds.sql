e779c61f894a1499f60c19a79abb1c06eba36524
Steve Kindt <stevemkindt@gmail.com>
2020-03-21 14:29:01 -0500

    database

---
+++ db/seeds.sql
@@ -0,0 +1,20 @@
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ("Cheeseburger", false);
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ("Bacon Cheeseburger", false);
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ("California Burger", false);
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ("Breakfast Burger", false);
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ("BBQ Burger", false);
